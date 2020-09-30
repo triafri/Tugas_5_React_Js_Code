2c682c54b5f1f86a91ef5c293f9cc98f510bab7e
TRI AFRILIYANTI <71365606+triafri@users.noreply.github.com>
2020-09-30 12:19:12 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MenuMakanan from './Page/MenuMakanan';
+
+function App() {
+  return (
+    <div className="App">
+      <MenuMakanan/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Page/MenuMakanan.js
@@ -0,0 +1,85 @@
+import React, {Component} from 'react';
+import DaftarMakanan from '../Lib/DaftarMakanan';
+import FormMakanan from '../Form/FormMakanan';
+
+const listmakanan=[
+    {
+        NamaMakanan:'Nasi Padang',
+        Harga:'20.000',
+   },
+    {
+        NamaMakanan:'Sate',
+        Harga:'20.000',
+    },
+    {
+        NamaMakanan:'Soto',
+        Harga:'10.000',
+    },
+    {
+        NamaMakanan:'Uduk',
+        Harga:'20.000',
+    },
+    {
+        NamaMakanan:'Nasi Kuning',
+        Harga:'15.000',
+    },
+    {
+        NamaMakanan:'Ayam Geprek',
+        Harga :'15.000'
+    },
+    {
+        NamaMakanan:'Pecel Lele',
+        Harga :'15.000'
+    },
+    {
+        NamaMakanan:'Pecel',
+        Harga :'15.000'   
+    },
+    {
+        NamaMakanan:'Mie Ayam',
+        Harga :'10.000'
+    }
+]
+
+class MenuMakanan extends Component{
+    render(){
+        return(
+            <div>
+                <h3>Daftar Makanan Favorit</h3>
+                <table align='center'>
+                    <tbody>
+                        <tr>
+                            {DaftarMakanan.map((data, index)=>{
+                                return(
+                                    <td key={index}>
+                                        <img src={data.img}
+                                        alt='Product Makanan'
+                                        width='150'
+                                        height='100'/>
+                                        <center>
+                                            <p>{data.NamaMakanan}</p>
+                                            <p>Harga : Rp. {data.Harga}</p>
+                                        </center>
+                                    </td>
+                                )
+                            })}
+                        </tr>
+                    </tbody>
+                </table>
+                <h4>List Makanan</h4>
+                {listmakanan.map((data, index)=>{
+                    return(
+                        <div key={index}>
+                            <p>
+                                {data.NamaMakanan} | Harga Rp. {data.Harga}
+                            </p>
+                        </div>
+                    )
+                })}
+                <FormMakanan/>
+            </div>
+        )
+    }
+}
+
+export default MenuMakanan
